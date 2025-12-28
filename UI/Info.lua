9e52af6fe7f4414a905ba9ad65aa448aa69258aa
capzk <capzk@outlook.com>
2025-12-28 20:52:33 +0800

    更新版本号至 1.1.3-rev1
    - 更新 CrateTrackerZK.toc 中的版本号
    - 更新 UI/Info.lua 中的回退版本号

--- UI/Info.lua
+++ UI/Info.lua
@@ -18,7 +18,7 @@ local ADDON_NAME = "CrateTrackerZK";
 
 local function GetAddonVersion()
     if not GetAddOnMetadata then
-        return "1.1.3";
+        return "1.1.3-rev1";
     end
     
     local version = GetAddOnMetadata(ADDON_NAME, "Version");
@@ -36,7 +36,7 @@ local function GetAddonVersion()
         end
     end
     
-    return "1.1.3";
+    return "1.1.3-rev1";
 end
 
 local function BuildAboutText()
